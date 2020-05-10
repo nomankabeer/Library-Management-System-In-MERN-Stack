b7a01bf509b177533576a99a651441ea9707a919
Noman Kabeer <44933511+nomankabeer@users.noreply.github.com>
2020-05-10 23:46:38 +0500

    working on auth routes
    working on auth routes

---
+++ server/routes/AuthRoutes.js
@@ -0,0 +1,15 @@
+const express = require ('express');
+const router = express.Router();
+const UserModel = require('../models/User');
+
+const AuthValidation = require('../request_validation/Auth')
+const AuthController = require('../controllers/AuthController')
+
+
+router.post('/register', AuthValidation.UserExist , AuthController.UserRegister)
+router.post('/login', AuthController.UserLogin)
+
+var passport = require('passport');
+router.post('/user' , [passport.authenticate('jwt', {session: false}) ] , AuthController.GetUser)
+
+module.exports = router;
